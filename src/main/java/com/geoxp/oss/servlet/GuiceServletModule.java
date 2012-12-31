abc65c570400c1fa767cc76ae6bd80b509b2981d
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2012-12-31 20:00:50 +0100

    Added servlet mappings.

--- src/main/java/com/geoxp/oss/servlet/GuiceServletModule.java
+++ src/main/java/com/geoxp/oss/servlet/GuiceServletModule.java
@@ -5,6 +5,10 @@ import com.google.inject.servlet.ServletModule;
 public class GuiceServletModule extends ServletModule {
   @Override
   protected void configureServlets() {
-    //serve("").with(.class);
+    serve("/GenMasterSecret").with(GenMasterSecretServlet.class);
+    serve("/Init").with(InitServlet.class);
+    serve("/GetSecret").with(GetSecretServlet.class);
+    serve("/GenSecret").with(GenSecretServlet.class);
+    serve("/GetOSSRSA").with(GetOSSRSAServlet.class);
   }
 }
