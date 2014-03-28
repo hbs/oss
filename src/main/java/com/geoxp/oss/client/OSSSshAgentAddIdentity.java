c1404039981bf42bdeff7b504438edfd057e4cac
gidouin <gidouin@gmail.com>
2014-03-28 13:22:03 +0100

    Don't trim() anymore the passphrase after unwrapping

--- src/main/java/com/geoxp/oss/client/OSSSshAgentAddIdentity.java
+++ src/main/java/com/geoxp/oss/client/OSSSshAgentAddIdentity.java
@@ -65,7 +65,7 @@ public class OSSSshAgentAddIdentity {
     byte[] secret = OSSClient.getSecret(args[0], args[1], null);
     // Use the secret to unwrap the passphrase
     byte[] unwrap = CryptoHelper.unwrapBlob(secret, Hex.decode(args[3]));
-    String password = new String(unwrap, "UTF-8").trim();
+    String password = new String(unwrap, "UTF-8");
 
     // Read private keys
     // openssh store it in PEM format		
