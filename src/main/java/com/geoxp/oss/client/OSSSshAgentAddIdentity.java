008ed59d3b7ee947aee2d94afb441629b706a126
lambour <sebastien.lambour@cityzendata.com>
2015-08-12 16:39:23 +0200

    adds a embeddedProvider BC available within the fatJar PEMReader and PEMUtilities have been cloned and adapted from BouncyCastle
     Activated only with the option ProviderEmbedded on the command line otherwise it use a regular installed provider

--- src/main/java/com/geoxp/oss/client/OSSSshAgentAddIdentity.java
+++ src/main/java/com/geoxp/oss/client/OSSSshAgentAddIdentity.java
@@ -1,22 +1,19 @@
 package com.geoxp.oss.client;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.*;
 import java.security.KeyPair;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Hex;
 import org.bouncycastle.openssl.EncryptionException;
-import org.bouncycastle.openssl.PEMReader;
 import org.bouncycastle.openssl.PasswordFinder;
 
 import com.geoxp.oss.CryptoHelper;
 import com.geoxp.oss.CryptoHelper.SSHAgentClient;
 import com.geoxp.oss.CryptoHelper.SSHAgentClient.SSHKey;
-import com.geoxp.oss.client.OSSClient;
 
 public class OSSSshAgentAddIdentity {
   private static class DefaultPasswordFinder implements PasswordFinder {
@@ -54,12 +51,13 @@ public class OSSSshAgentAddIdentity {
    */
   public static void main(String[] args) throws Exception {
     if (args.length < 3) {
-      System.err.println("Usage: OSSLoadAgent OSS_URL SECRET_NAME WRAPPED_PASSPHRASE AGENT_AUTH_SOCK [KEY_FILE]");
+      System.err.println("Usage: OSSLoadAgent OSS_URL SECRET_NAME AGENT_AUTH_SOCK WRAPPED_PASSPHRASE [ProviderEmbedded default false] [KEY_FILE]");
       System.exit(1);
     }
 
     SSHAgentClient sshAgent = new SSHAgentClient(args[2]);
 
+    System.out.println("Unwrap secret");
     // Get the secret from OSS
     // FIXME ? Provide a way to specify the ssh signing key fingerprint
     byte[] secret = OSSClient.getSecret(args[0], args[1], null);
@@ -67,45 +65,83 @@ public class OSSSshAgentAddIdentity {
     byte[] unwrap = CryptoHelper.unwrapBlob(secret, Hex.decode(args[3]));
     String password = new String(unwrap, "UTF-8");
 
+    boolean providerEmbedded = false;
+
     // Read private keys
-    // openssh store it in PEM format		
-    List<File> sshKeyFiles;
-    if (args.length > 4) {
-      sshKeyFiles = new ArrayList<File>(1);
-      sshKeyFiles.add(new File(args[4]));
-    } else {
+    // openssh store it in PEM format
+    List<File> sshKeyFiles = new ArrayList<File>(1);
+
+    for (int i=4; i<args.length; i++) {
+      if ("ProviderEmbedded".equals(args[i])) {
+        providerEmbedded = true;
+      } else {
+        sshKeyFiles.add(new File(args[i]));
+      }
+    }
+
+    // not key founded take the default keyFiles
+    if (sshKeyFiles.size() == 0){
       sshKeyFiles = getDefaultsKeyFiles();
     }
 
     for (File sshKeyFile : sshKeyFiles) {
       Reader fRd = new BufferedReader(new FileReader(sshKeyFile));
-      PEMReader pem = new PEMReader(fRd, new DefaultPasswordFinder(password.toCharArray()), "BC");
 
-      Object o;
-      try {
-        while ((o = pem.readObject()) != null) {
-          if (o instanceof KeyPair) {
-            KeyPair kp = (KeyPair) o;
-            // Add the identity in the ssh-agent
-            byte[] keyblob = CryptoHelper.sshPrivateKeyBlobFromKeyPair(kp);
-            System.out.println("Loading " + sshKeyFile.getPath());
-            sshAgent.addIdentity(keyblob, sshKeyFile.getPath());
+      if (providerEmbedded) {
+        // load with embedded provider
+        System.out.println("Load PEM file " + sshKeyFile.getName() + " with embedded provider"  );
+        BouncyCastleProvider bouncyCastleProvider = new BouncyCastleProvider();
+        com.geoxp.oss.client.bouncycastle.openssl.PEMReader pem = new com.geoxp.oss.client.bouncycastle.openssl.PEMReader(fRd, new DefaultPasswordFinder(password.toCharArray()), bouncyCastleProvider);
+        Object o;
+        try {
+          while ((o = pem.readObject()) != null) {
+            if (o instanceof KeyPair) {
+              loadKeyPair((KeyPair) o, sshKeyFile, sshAgent);
+            }
           }
+        } catch (EncryptionException ee) {
+          System.err.println("Can't read private key in " + sshKeyFile.getAbsolutePath());
+          ee.printStackTrace();
+        } finally {
+          pem.close();
         }
-      } catch (EncryptionException ee) {
-        System.err.println("Can't read private key in " + sshKeyFile.getAbsolutePath());
-        ee.printStackTrace();
+      } else {
+        // load with signed javax security provider
+        System.out.println("Load PEM file " + sshKeyFile.getName() + " with JCE provider"  );
+        org.bouncycastle.openssl.PEMReader pem = new org.bouncycastle.openssl.PEMReader(fRd, new DefaultPasswordFinder(password.toCharArray()), "BC");
+        Object o;
+        try {
+          while ((o = pem.readObject()) != null) {
+            if (o instanceof KeyPair) {
+              loadKeyPair((KeyPair) o, sshKeyFile, sshAgent);
+            }
+          }
+        } catch (EncryptionException ee) {
+          System.err.println("Can't read private key in " + sshKeyFile.getAbsolutePath());
+          ee.printStackTrace();
+        } finally {
+          pem.close();
+        }
       }
-
-      pem.close();
     }
 
     System.out.println("Keys in agent:");
+
     List<SSHKey> identities = sshAgent.requestIdentities();
     for (SSHKey identity : identities) {
       System.out.println(identity);
     }
-
   }
 
+  private static void loadKeyPair(KeyPair kp, File sshKeyFile, SSHAgentClient sshAgent) {
+    // Add the identity in the ssh-agent
+    try {
+      byte[] keyblob = CryptoHelper.sshPrivateKeyBlobFromKeyPair(kp);
+      System.out.println("Loading " + sshKeyFile.getPath());
+      sshAgent.addIdentity(keyblob, sshKeyFile.getPath());
+    } catch (IOException e) {
+      System.err.println("Can't read private key in " + sshKeyFile.getAbsolutePath());
+      e.printStackTrace();
+    }
+  }
 }
